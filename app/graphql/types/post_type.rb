80f182d17302ccdd799b82959070a93ebd424081
Manomoy Biswas <manomoy.biswas@bitcanny.com>
2024-01-05 01:07:33 +0530

    Quering added

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -3,7 +3,6 @@
 module Types
   class PostType < Types::BaseObject
     field :id, ID, null: false
-    field :user_id, Integer, null: false
     field :title, String
     field :body, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
